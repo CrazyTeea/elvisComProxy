cdd4328712f02b3a71ecac168af3493efa2d93cd
CrazyTea <lipatow.nikita@yandex.ru>
2024-09-04 09:48:03 +0300

    750ms

--- ElvisComProxy/Program.cs
+++ ElvisComProxy/Program.cs
@@ -27,7 +27,7 @@ namespace ElvisComProxy
 
         public int GetLength()
         {
-            return Length ?? 450;
+            return Length ?? 750;
         }
 
         private Thread? _thread;
